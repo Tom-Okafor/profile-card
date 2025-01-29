3aa2855364398514148dfc45926d995f57a82d73
Tom Okafor <tmeka008@gmail.com>
2025-01-29 19:36:45 +0100

    update styles.

--- scripts/index.js
+++ scripts/index.js
@@ -4,5 +4,23 @@ function caliberateCardHeight() {
   document.querySelector(".border").style.height = `${height * (103 / 100)}px`;
 }
 
+function setUTCTime() {
+  const DATE = new Date();
+  const HOUR = DATE.getUTCHours();
+  const MINUTES = DATE.getUTCMinutes();
+  let convertedHour;
+  let dayPeriod;
+  if (HOUR > 12) {
+    convertedHour = HOUR - 12;
+    dayPeriod = "PM";
+  } else {
+    convertedHour = HOUR;
+    dayPeriod = "AM";
+  }
+  return `${convertedHour} : ${MINUTES} ${dayPeriod}`;
+}
+
+document.getElementById("time").innerText = setUTCTime();
+
 window.addEventListener("resize", caliberateCardHeight);
 caliberateCardHeight();
